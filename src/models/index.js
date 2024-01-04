ff63c8769d66b9667bc712bae3187a5c261d8e0f
sindhuasri <ssridharan@skillstorm.com>
2024-01-03 20:55:46 -0500

    x draft

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Individual = require('./contacts/individual.model');
+module.exports.X = require('./x.model');
