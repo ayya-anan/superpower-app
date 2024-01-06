cc10f7ec4ebaeff65f7622738a768d60759db9a0
Jerin Jose Johnson <jerin.j.johnson@virtana.com>
2024-01-06 16:20:53 +0530

    added orginization APIs

--- src/config/enums.js
+++ src/config/enums.js
@@ -2,10 +2,12 @@ const statusEmuns = ['active', 'inactive', 'deleted'];
 const addressTypeEmuns = ['home', 'office'];
 const phoneTypeEmuns = ['home', 'office', 'personal'];
 const socialMediaTypeEmuns = ['facebook', 'linkedin', 'github'];
+const orgStatusEmuns = ['Prospect', 'Active', 'Inactive', 'Suspended'];
 
 module.exports = {
   statusEmuns,
   addressTypeEmuns,
   phoneTypeEmuns,
   socialMediaTypeEmuns,
+  orgStatusEmuns,
 };
--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Individual = require('./contacts/individual.model');
+module.exports.Organization = require('./contacts/organization.model');
