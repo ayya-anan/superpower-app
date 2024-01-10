1c22438505f265b69dc1a0bef6e7368213c82092
sindhuasri <ssridharan@skillstorm.com>
2024-01-09 19:12:32 -0500

    x crud

--- src/models/x.model.js
+++ src/models/x.model.js
@@ -3,21 +3,16 @@ const mongoose = require('mongoose');
 const { toJSON, paginate } = require('./plugins');
 
 const xSchemas = new Map();
+const models = require('./x');
 
-const xSchema = mongoose.Schema({
-  personalDetails: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+models.forEach((m) => {
+  // eslint-disable-next-line no-console
+  const xSchema = mongoose.Schema(m.model);
+  // add plugin that converts mongoose to json
+  xSchema.plugin(toJSON);
+  xSchema.plugin(paginate);
+  const x = mongoose.model(m.type[0].toUpperCase() + m.type.slice(1).toLowerCase(), xSchema);
+  xSchemas.set(m.type, x);
 });
-
-// add plugin that converts mongoose to json
-xSchema.plugin(toJSON);
-xSchema.plugin(paginate);
-
-const x = mongoose.model('Test', xSchema);
-
-xSchemas.set('test', x);
 
 module.exports = xSchemas;
