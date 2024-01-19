e0af0435ea240328484c92215aa66a49a1acf339
Jerin Jose Johnson <jerin.j.johnson@virtualinstruments.com>
2024-01-19 11:03:12 +0530

    added deals model

--- src/models/x.js
+++ src/models/x.js
@@ -42,6 +42,51 @@ const x = [
       },
     },
   },
+  {
+    type: 'deal',
+    model: {
+      dealName: { type: String, required: true },
+      org: { type: String, required: true },
+      status: { type: String, required: true },
+      customerContact: { type: String, required: true },
+      winProbablity: String,
+      accountManager: { type: String, required: true },
+      startDate: { type: Date, required: true },
+      source: String,
+      value: { type: Number, required: true },
+      closeDate: { type: Date, required: true },
+      quotes: [
+        {
+          date: { type: Date, required: true },
+          status: { type: String, required: true },
+          subTotal: { type: Number, required: true },
+          vat: { type: String, required: true },
+          discount: { type: String, required: true },
+          total: { type: Number, required: true },
+          paymentMilestone: { type: String },
+          services: [
+            {
+              facility: { type: String, required: true },
+              service: { type: String, required: true },
+              unitRate: { type: Number, required: true },
+              quantity: { type: Number, required: true },
+              employeeCount: { type: Number, required: true },
+              total: { type: Number, required: true },
+            },
+          ],
+          payments: [
+            {
+              date: { type: Date, required: true },
+              criteria: { type: String, required: true },
+              percentage: { type: String, required: true },
+              amount: { type: String, required: true },
+              status: { type: String, required: true },
+            },
+          ],
+        },
+      ],
+    },
+  },
 ];
 
 module.exports = x;
