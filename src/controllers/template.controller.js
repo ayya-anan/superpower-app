7d22b102c381e6c5649991d77cad1199456844e8
Jerin Jose <jerin.j.johnson@opscruise.com>
2024-01-24 10:39:07 +0530

    email is added

--- src/controllers/template.controller.js
+++ src/controllers/template.controller.js
@@ -1,17 +1,51 @@
-const createReport = require('docx-templates').default;
-const fs = require('fs');
+// const createReport = require('docx-templates').default;
+const he = require('he');
+// const fs = require('fs');
+const nodemailer = require('nodemailer');
+const httpStatus = require('http-status');
 
-const template = fs.readFileSync('./src/templates/invoice.docx');
+const config = require('../config/config');
+const logger = require('../config/logger');
+
+const transport = nodemailer.createTransport(config.email.smtp);
+
+/* istanbul ignore next */
+if (config.env !== 'test') {
+  transport
+    .verify()
+    .then(() => logger.info('Connected to email server'))
+    .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
+}
+
+/**
+ * Send an email
+ * @param {string} to
+ * @param {string} subject
+ * @param {string} text
+ * @returns {Promise}
+ */
+const sendEmail = async (to, subject, text, html = false) => {
+  const msg = { from: config.email.from, to, subject, text, html, headers: { 'Content-Type': 'text/html' } };
+  await transport.sendMail(msg);
+};
+
+// const template = fs.readFileSync('./src/templates/invoice.docx');
 const catchAsync = require('../utils/catchAsync');
 
 const createInvoice = catchAsync(async (req, res) => {
-  const buffer = await createReport({
-    template,
-    data: req.body,
-  });
+  const subject = 'Quote';
+  const to = 'jerinjose.j@gmail.com';
+  const content = he.decode(req.body.content);
+  await sendEmail(to, subject, '', content);
+  res.status(httpStatus.NO_CONTENT).send();
 
-  fs.writeFileSync('report.docx', buffer);
-  res.send(fs);
+  // const buffer = await createReport({
+  //   template,
+  //   data: req.body,
+  // });
+
+  // fs.writeFileSync('report.docx', buffer);
+  // res.send(fs);
 });
 
 module.exports = {
