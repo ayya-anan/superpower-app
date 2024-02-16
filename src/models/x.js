bda9939aa98ddb237dd53fe6ad61cdb8f4b68250
Natarajan Ramamoorthy <mailtonadesh.nr@gmail.com>
2024-02-16 18:31:00 +0530

    Task Allocation API added for Bug

--- src/models/x.js
+++ src/models/x.js
@@ -96,7 +96,7 @@ const x = [
       allocationPercentage: { type: Number, required: true },
       startDate: { type: Date, required: true },
       endDate: { type: Date, required: true },
-      id: { type: String, required: false },
+      id: { type: String, required: true },
       orgId: { type: String, required: true },
       name: { type: String, required: true },
       project: { type: String, required: true },
