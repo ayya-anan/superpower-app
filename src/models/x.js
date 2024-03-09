600a84e135e41ee749a7b0cd757ac13273d1bce3
Jerin <jerinjose.j@gmail.com>
2024-03-09 05:45:21 +0530

    Add Deal model, service, controller, and route

--- src/models/x.js
+++ src/models/x.js
@@ -45,54 +45,6 @@ const x = [
     },
   },
   {
-    type: 'deal',
-    model: {
-      dealName: { type: String, required: true },
-      org: { type: String, required: true },
-      status: { type: String, required: true },
-      customerContact: { type: String },
-      winProbablity: String,
-      accountManager: { type: String },
-      startDate: { type: Date, required: true },
-      source: String,
-      type: { type: String },
-      value: { type: Number, required: true },
-      closeDate: { type: Date, required: true },
-      quotes: [
-        {
-          date: { type: Date, required: true },
-          status: { type: String, required: true },
-          type: { type: String },
-          paymentType: { type: String },
-          subTotal: { type: Number, required: true },
-          vat: { type: Number, required: true },
-          discount: { type: String, required: true },
-          total: { type: Number, required: true },
-          paymentMilestone: { type: Number },
-          services: [
-            {
-              facility: { type: String, required: true },
-              service: { type: String, required: true },
-              unitRate: { type: Number, required: true },
-              quantity: { type: Number, required: true },
-              employeeCount: { type: Number, required: true },
-              total: { type: Number, required: true },
-            },
-          ],
-          payments: [
-            {
-              date: { type: Date, required: true },
-              criteria: { type: String, required: true },
-              percentage: { type: String, required: true },
-              amount: { type: String, required: true },
-              status: { type: String, required: true },
-            },
-          ],
-        },
-      ],
-    },
-  },
-  {
     type: 'taskAllocation',
     model: {
       allocatedHours: { type: Object, required: true },
--- src/services/index.js
+++ src/services/index.js
@@ -5,3 +5,4 @@ module.exports.userService = require('./user.service');
 module.exports.xService = require('./x.service');
 module.exports.individualService = require('./contacts/indivdual.service');
 module.exports.organizationService = require('./contacts/organization.service');
+module.exports.dealService = require('./leads/deal.service');
