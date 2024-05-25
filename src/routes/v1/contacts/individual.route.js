820f393653088cb88a207d8a71926e4360c0a8b4
Natarajan Ramamoorthy <mailtonadesh.nr@gmail.com>
2024-05-25 18:00:47 +0530

    Changes for adding Allocation Details API - TimeSheet

--- src/routes/v1/contacts/individual.route.js
+++ src/routes/v1/contacts/individual.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const validate = require('../../../middlewares/validate');
 const individualValidation = require('../../../validations/contacts/individual.validation');
 const individualController = require('../../../controllers/contacts/individual.controller');
+const allocationController = require('../../../controllers/contacts/task-allocation.controller');
 
 const router = express.Router();
 
@@ -15,5 +16,9 @@ router
   .get(validate(individualValidation.getIndividual), individualController.getIndividual)
   .patch(validate(individualValidation.updateIndividual), individualController.updateIndividual)
   .delete(validate(individualValidation.deleteIndividual), individualController.deleteIndividual);
+
+router
+  .route('/getTotalHours/')
+  .post(allocationController.getTotalHours);
 
 module.exports = router;
